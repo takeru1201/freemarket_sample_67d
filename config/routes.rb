e5933c12fa53a69d63bfd1297d1da4ab23845c10
takeru1201 <ldn.asktmhmh@icloud.com>
2020-04-13 22:10:13 +0900

    devise controllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
   root 'items#index'
   root to: 'home#index'
   resources :users, only: :show
