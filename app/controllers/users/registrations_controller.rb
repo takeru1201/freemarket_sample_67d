0471ac43e6f774d5cedd51641d7ea303f765b0bf
takeru1201 <ldn.asktmhmh@icloud.com>
2020-04-18 12:34:10 +0900

    bundle:updaeteの実行

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,7 +6,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user = User.new
     @address = @user.build_address
   end
-  # before_action :configure_sign_up_params, only: [:create]
+
+
+  # # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
