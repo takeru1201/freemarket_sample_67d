29ffe891b1706dc5fd2323f3d1a467cf0abfad0a
takeru1201 <ldn.asktmhmh@icloud.com>
2020-04-18 19:14:00 +0900

    追加:正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,53 @@ class User < ApplicationRecord
   has_one :credit_card
   has_one :address
   accepts_nested_attributes_for :address
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  VALID_KATAKANA_REGEX = /\A[\p{katakana}\p{blank}ー－]+\z/
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d!@#\$%\^\&*\)\(+=._-]{7,128}\z/i
+  VALID_POSTAL_CODE = /\A\d{3}-\d{4}\z/i
+
+#registration
+  validates :nickname, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: 'のフォーマットが不適切です'}
+  validates :password, presence: true, length: { in: 7..128 }, format: { with: VALID_PASSWORD_REGEX, message: 'は英字と数字両方を含むパスワードを設定してください'}
+  validates :password_confirmation, presence: true, length: { in: 7..128 }, format: { with: VALID_PASSWORD_REGEX, message: 'は英字と数字両方を含むパスワードを設定してください'}
+  validates :last_name, presence: true, length: { maximum: 35 }
+  validates :first_name, presence: true, length: { maximum: 35 }
+  validates :last_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'はなで肩です'}
+  validates :first_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
+  # validates :birth_yyyy_id, presence: true
+  # validates :birth_mm_id, presence: true
+  # validates :birth_dd_id, presence: true
+
+#sms_confirmation
+  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/, message: 'の入力が正しくありません'}
+
+#sms_confirmation/sms
+  # validates :authentication_num, presence: true, numericality: { only_integer: true }
+
+#signup/address
+  validates :last_name, presence: true, length: { maximum: 35 }
+  validates :first_name, presence: true, length: { maximum: 35 }
+  validates :last_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
+  validates :first_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
+  # validates :postal_code, presence: true, length: { maximum: 8 }, format: { with: VALID_POSTAL_CODE, message: 'のフォーマットが不適切です' }
+  # validates :prefectures, presence: true, numericality: { only_integer: true, less_than: 49 }
+  # validates :municipality, presence: true, length: { maximum: 50 }
+  # validates :house_number, presence: true, length: { maximum: 100 }
+
+  def full_name
+    "#{self.last_name} #{self.first_name}"
+  end
+
+  def full_name_kana
+    "#{self.last_name_kana} #{self.first_name_kana}"
+  end
+
+  # yyyy/mm/dd の形式で表示
+  # def birthday
+  #   "#{BirthYyyy.find(self.birth_yyyy_id).year}/#{BirthMm.find(self.birth_mm_id).month}/#{BirthDd.find(self.birth_dd_id).day}"
+  # end
 end
+
